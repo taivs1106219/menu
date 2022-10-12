381811c4118a60f3ae4fc69ec051617d5a9ee12e
ryantsui1109 <68107903+ryantsui1109@users.noreply.github.com>
2022-10-12 13:23:41 +0800

    added gitignore file

--- menu.cpp
+++ menu.cpp
@@ -8,16 +8,18 @@
 using namespace std;
 void f01();
 void f02();
+void f03();
+void f04();
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char **argv)
 {
 	string menuItem[] = {
-		"[1]三數平均",
-		"[2]",
-		"[3]",
-		"[4]",
+		"[1] 三數平均",
+		"[2] P75Q3",
+		"[3]HW:20221005 取個位數",
+		"[4]HW:221005 算土產",
 		"[5]",
 		"[6]",
 		"[7]",
@@ -46,13 +48,20 @@ int main(int argc, char **argv)
 		case 2:
 			f02();
 			break;
+
+		case 3:
+			f03();
+			break;
+
+		case 4:
+			f04();
+			break;
 		}
 		cout << "\n";
 		system("pause");
 		return 0;
 	}
 }
-
 void f01()
 {
 	int chi, eng, math;
@@ -65,5 +74,38 @@ void f01()
 
 void f02()
 {
-	cout << "空空如也";
+	cout << "3+5=" << 3 + 5 << endl;
+	cout << "3%5=" << 3 % 5 << endl;
+
+	int i = 1;
+	i += ++i;
+	cout << "i=1;i+=++i;i=" << i << endl;
+
+	cout << sizeof(int) << endl;
+	cout << sizeof 3.14 << endl;
+	cout << sizeof "a" << endl;
+}
+
+void f03()
+{
+	unsigned int num;
+	unsigned int digits;
+	cout << "輸入正整數：";
+	cin >> num;
+	digits = num % 10;
+	cout << num << "的個位數是" << digits << endl;
+}
+void f04()
+{
+	
+
+	int people;
+	int boxes;
+	int singles;
+	cout << "請輸入班級人數";
+	cin >> people;
+	boxes = people / 8;
+	singles = people - (8 * boxes);
+	cout << "買" << boxes << "盒，單買" << singles << "個" << endl;
+	cout << "共計" << (boxes * 12) + (singles * 2) << "元" << endl;
 }
